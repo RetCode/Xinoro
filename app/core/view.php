0c1bf622933c4566d337a1e7da9dc68ce8b5c5fd
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-09-03 23:29:55 +0300

    Insert comment

--- app/core/view.php
+++ app/core/view.php
@@ -2,12 +2,27 @@
 
     namespace app\core;
 
-    /* Родительский класс всех представлений */
-    /* by xoheveras                          */
+    /**
+    *|-----------------------------------------------------------------
+    *|
+    *|  This file contains the parent class of all views,
+    *|  used to launch the page and integrate variables into it.
+    *|
+    *|  by xoheveras
+    *|-----------------------------------------------------------------
+    **/
+
     class View
     {
         protected $controller;
         protected $action;
+
+        /**
+        *|-----------------------------------------------------------------
+        *|  @param string $controller - Name controller in folder
+        *|  @param string $action - Name action(func) in controller(up)
+        *|-----------------------------------------------------------------
+        **/
 
         function __construct($controller,$action)
         {
@@ -15,15 +30,16 @@
             $this->action = $action;
         }
 
+        /**
+        *|-----------------------------------------------------------------
+        *|  @param array $args - Contains variables that will be unpacked
+        *|  on the page
+        *|-----------------------------------------------------------------
+        **/
+
         function run($args = [])
         {
             extract($args);
             require_once("app/views/".$this->controller."/".$this->action.".php");
-        }
-
-        function preload($controller, $action, $args = [])
-        {
-            extract($args);
-            require_once("app/views/".$controller."/".$action.".php");
         }
     }

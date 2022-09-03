b807ff6200f61b0ace1fac65be7d03318aaa69fd
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-09-03 23:37:03 +0300

    Insert comment

--- app/core/model.php
+++ app/core/model.php
@@ -2,9 +2,23 @@
 
     namespace app\core;
 
-    /* Родительский класс всех моделей      */
-    /* by xoheveras                         */
+    /**
+    *|-----------------------------------------------------------------
+    *|
+    *|  This file contains the parent class of all models, used
+    *|  to write functionality on the site, implementing the MVC
+    *|  design method.
+    *|
+    *|  by xoheveras
+    *|-----------------------------------------------------------------
+    **/
+
     class Model
     {
-        
+    	protected DataBase;
+
+        function __construct()
+        {
+            $this->DataBase = new DataBase();
+        }
     }
